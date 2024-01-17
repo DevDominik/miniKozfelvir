55ae11978aefe7cd8815b78548a5c2f7d9548f60
DevDominik <71924117+DevDominik@users.noreply.github.com>
2024-01-17 14:35:48 +0100

    Add files via upload

---
+++ miniKozfelvir/Felvetelizo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace miniKozfelvir
+{
+    public class Felvetelizo
+    {
+        public const string CSVFEJ = "om_azonosito;nev;email;szuletesi_datum;ertesitesi_cim;matek_eredmeny;magyar_eredmeny";
+
+        string omAzon;
+        string nev;
+        string email;
+        DateTime szuletesiDatum;
+        string ertesitesiCim;
+        byte? matek;
+        byte? magyar;
+
+        public Felvetelizo(string omAzon, string nev, string email, DateTime szuletesiDatum, string ertesitesiCim, byte? matek, byte? magyar)
+        {
+            this.omAzon = omAzon;
+            this.nev = nev;
+            this.email = email;
+            this.szuletesiDatum = szuletesiDatum;
+            this.ertesitesiCim = ertesitesiCim;
+            this.matek = matek;
+            this.magyar = magyar;
+        }
+
+        public Felvetelizo(string csvSor)
+        {
+            string[] mezok = csvSor.Split(';');
+
+            this.omAzon = mezok[0];
+            this.nev = mezok[1];
+            this.email = mezok[2];
+            this.szuletesiDatum = DateTime.Parse(mezok[3]);
+            this.ertesitesiCim = mezok[4];
+
+            byte _matek;
+            this.matek = Byte.TryParse(mezok[5], out _matek) ? (byte?)_matek : null;
+
+            byte _magyar;
+            this.magyar = Byte.TryParse(mezok[6], out _magyar) ? (byte?)_magyar : null;
+        }
+
+        public string OmAzon { get => omAzon; }
+        public string Nev { get => nev; }
+        public string Email { get => email; }
+        public DateTime SzuletesiDatum { get => szuletesiDatum; }
+        public string ErtesitesiCim { get => ertesitesiCim; }
+        public byte? Matek { get => matek; }
+        public byte? Magyar { get => magyar; }
+
+        public override string ToString()
+        {
+            return $"{omAzon};{nev};{email};{szuletesiDatum};{ertesitesiCim};{matek};{magyar}";
+        }
+    }
+}
---
+++ miniKozfelvir/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace miniKozfelvir
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Felvetelizo> felvetelizok;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Import(File.ReadAllLines("felvetelizok.csv"));
+        }
+
+        public void Import(string[] lines)
+        {
+            felvetelizok = lines.Skip(1).Select(x => new Felvetelizo(x)).ToList();
+        }
+
+        public void ShowImport() {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.DefaultExt = ".csv";
+            if (ofd.ShowDialog() == true) {
+                Import(File.ReadAllLines(ofd.FileName));
+            }
+        }
+
+        public void Export()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = ".csv";
+            if (sfd.ShowDialog() == true)
+            {
+                File.WriteAllLines(sfd.FileName, felvetelizok.Select(x => x.ToString()).Prepend(Felvetelizo.CSVFEJ).ToList());
+            }
+        }
+    }
+}
